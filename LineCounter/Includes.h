d9c7a748588f01f949a50cf3230b01f7218e2dda
spyrothelegend <mail@me.de>
2018-06-13 19:58:11 +0200

    Projektdateien hinzufügen.

---
+++ LineCounter/Includes.h
@@ -0,0 +1,18 @@
+#pragma once
+#include <iostream>
+#include <Windows.h>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <cctype>
+#include <locale>
+#include <algorithm> 
+#include <filesystem>
+#include <experimental/filesystem> 
+#include <map>
+#include <string_view>
+
+using namespace std;
+using namespace std::experimental::filesystem;
+namespace filesys = std::experimental::filesystem;
+
